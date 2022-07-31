52ad6b420728289cd6985e1bd89ce673176375bd
ArthurAscx <arthurascx@gmail.com>
2022-07-31 20:06:58 -0300

    Proyecto npm iniciado con express

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
-const peliculas = require("./movies.js");
-
-const moviesDH = {
-    peliculas: peliculas,
-
-    listMovies: () => peliculas.forEach((elemento) => console.log(elemento)),
-
-    searchMovie: (aBuscar) => {
-        let encontrada = peliculas.filter((pelicula) => ((pelicula.id === aBuscar) || (pelicula.title === aBuscar)));
-        return encontrada.length ? encontrada[0] : null
-    },
-
-    searchMovieByGenre: (genero) => {
-        let encontradas = peliculas.filter((pelicula) => (pelicula.genre === genero));
-        return encontradas.length ? encontradas : null;
-    },
-
-    totalPrice: () => peliculas.reduce((acumulador, elemento) => acumulador + elemento.price, 0),
-
-    changeMovieGenre: function(id, genero) {
-        let cambio = this.searchMovie(id);
-        //let cambio = moviesDH.searchMovie(id);
-        return cambio.genre = genero;
-    }
-
-};
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
 
 
-moviesDH.changeMovieGenre(3, "accion");
-console.log(moviesDH.listMovies());
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
