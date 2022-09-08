f66cdaa9bad26c089e2fbaa96837699ea4d1fbb3
ArthurAscx <arthurascx@gmail.com>
2022-09-07 22:20:50 -0300

    user handler

--- controllers/userHandler.js
+++ controllers/userHandler.js
@@ -1,11 +1,51 @@
 const path = require("path");
+const fs = require("fs");
+const rutaArchivo = path.join(__dirname, "/data/userList.json")
+
 const userHandler = {
-    login: (req,res)=>{
+    login: (req, res) => {
         res.render("login");
     },
-    register: (req,res)=>{
+    register: (req, res) => {
         res.render("register");
     },
+    crear: (req, res) => {
+        let usuarios = JSON.parse(fs.readFileSync(rutaArchivo, "utf-8"));
+        let usr = {
+            id: Date.now(),
+            Nombreapellido: req.body.nombreApellido,
+            Usuario: req.body.nombreDeUsuario,
+            Nacimiento: req.body.fechaDeNacimiento,
+            Email: req.body.correo,
+            Contraseña: req.body.pass,
+            Categoria: req.body.categoria,
+            Imagen: "OIP.jpg"
+        };
+        if (req.file){
+            usr.Imagen = req.file.filename
+        }
+        usuarios.push(usr);
+        usuarios = JSON.stringify(usuarios, null, " ");
+        fs.writeFileSync(rutaArchivo, usuarios);
+        res.redirect("/usuario/login")
+    },
+    lista: (req,res)=>{
+        let usuarios = JSON.parse(fs.readFileSync(rutaArchivo, "utf-8"));
+        res.render("userList", {users: usuarios})
+    },
+    edicion: (req, res)=>{
+        let usuarios = JSON.parse(fs.readFileSync(rutaArchivo, "utf-8"));
+        // Usar el find para encontrar el usuario y luego mandarlo a la página
+        let userid = parseInt(req.params.id);
+        let user = usuarios.find((u)=> u.id === userid);
+        if (user){
+        res.render("userEdit", {user})
+        }
+        else{
+            console.log(userid);
+            res.send(user)
+        }
+    }
 };
 
 module.exports = userHandler;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,17 +1,31 @@
 const express = require("express");
-
 const router = express.Router();
-
 const rutasUsuario= require("../controllers/userHandler");
+const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../public/img/Avatares'))
+    },
+    filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname)
+      const idAvatar = Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + idAvatar + ext)
+    }
+  })
+ const upload = multer({storage});
 
-//router.get("/detalle", rutasUsuario.perfil);
 router.get("/login", rutasUsuario.login);
 router.get("/register", rutasUsuario.register);
-
+router.post("/crear", upload.single("avatar"), rutasUsuario.crear)
+router.get("/lista", rutasUsuario.lista)
+router.get("/edicion/:id", rutasUsuario.edicion)
 
 module.exports = router;
 
 /* Luego chequear esto
 router.get("/user/:id", rutasUsuario.perfil);
+//router.get("/detalle", rutasUsuario.perfil);
 router.get("/carrito/:userid", rutasProducto.carrito);
 */
+
