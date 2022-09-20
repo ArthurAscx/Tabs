5737a75fa676427a436c0b282afbc7823dc4daee
ventas1 <ventas1@RACECOTT.LOCAL>
2022-09-20 15:54:38 -0300

    Modificando los errores

--- middlewares/userRegister.js
+++ middlewares/userRegister.js
@@ -6,7 +6,9 @@ const validacion = [
     body("nombreDeUsuario").notEmpty().withMessage("Tienes que escribir un nombre de usuario"),
     body("fechaDeNacimiento").notEmpty().withMessage("Tienes que escribir una fecha de nacimiento"), 
     body("avatar").notEmpty().withMessage("Tienes subier una imagen"), 
-    body("correo").notEmpty().withMessage("Tienes que escribir un correo"), 
+    body("correo")
+        .notEmpty().withMessage("Tienes que escribir un correo").bail()
+        .isEmail().withMessage("Tienes que escribir un correo válido"),
     body("categoria").notEmpty().withMessage("Tienes que elegir una categoria"),
     body("password").notEmpty().withMessage("Tienes que elegir una constraseña"),
     body("password").notEmpty().withMessage("Tienes que elegir una constraseña"),
