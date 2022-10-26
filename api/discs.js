bbb1b14c8ece374b7fb4b7a2a232e29b5377b8b8
ArthurAscx <arthurasc@outlook.com>
2022-10-26 18:53:39 -0300

    create del disk

--- api/discs.js
+++ api/discs.js
@@ -4,18 +4,48 @@ let disc = {
 
     all: async function (req, res) {
         try {
-            let allDiscs = await db.Disc.findAll()
-            res.send(allDiscs.JSON())
+            let allDiscs = await db.Disc.findAll();
+            allDiscs ?
+            res.JSON({
+                total: allDiscs.length,
+                data: allDiscs,
+                status: 200
+            }) 
+            :
+            res.send("No se recibió informacion")
+
         } catch (error) {
             res.send("Error en la consulta")
         }
     },
+
     disc: async (req, res) => {
         try {
             let discFound = await db.Disc.findByPk(req.params.id);
-            discFound ? res.send(discFound): res.send("El disco no existe")
+            discFound ? res.json({
+                data: discFound,
+                status: 200
+            })
+            :
+            res.send("El disco no existe")
         } catch (error) {
             res.send("Error en la consulta")
+        }
+    },
+
+    newDisc: async (req,res)=>{
+        try {
+            let createdDisc = await db.Disc.create(req.body);
+            createdDisc ? res.json({
+                data: createdDisc,
+                status: 200
+            })
+            :
+            // Aquí teoricamente no llega nunca porque el sequelize tiene unas validaciones de campos no nulos que hacen que caiga en el catch
+            res.send("El disco creado esta vacio")
+        } catch (error) {
+            console.log(req.body);
+            res.send("Error al momento de crear un disco " + error)
         }
     }
 };
--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -6,6 +6,7 @@ const rutasAPI = require("../api/discs")
 
 router.get("/discs/all", rutasAPI.all);
 router.get("/discs/:id", rutasAPI.disc);
+router.post("/discs/create", rutasAPI.newDisc)
 //router.get("/contacto", rutasPrincipales.contacto);
 //router.get("/ayuda", rutasPrincipales.ayuda);
 
