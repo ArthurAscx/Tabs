dd899c39e198feb8d98d5388a96908d3e8678893
Pablo-Brizuela <pablo_2002_cab_@hotmail.com>
2022-10-31 13:58:57 -0300

    Creando const en controlador a database

--- controllers/productHandler.js
+++ controllers/productHandler.js
@@ -2,6 +2,8 @@ const path = require("path");
 const fs = require("fs");
 const rutaArchivo = path.join(__dirname, "/data/productList.json")
 const lista = JSON.parse(fs.readFileSync(rutaArchivo), "utf-8")
+const db = require("../Database/models")
+
 
 const productHandler = {
     detalle: (req, res) => {
