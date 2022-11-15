afe2e24c15df9fced5a21eea18f096cd94f8a2a0
ArthurAscx <arthurascx@gmail.com>
2022-11-15 20:39:03 -0300

    locura de back

--- middlewares/userValidator.js
+++ middlewares/userValidator.js
@@ -1,5 +1,7 @@
 const { body } = require("express-validator");
 const passUnencripted = body.password
+const path = require("path");
+const axios = require("axios")
 //validaciones
 const validacionRegister = [
     body("firstName").notEmpty().withMessage("Tienes que escribir un nombre").bail()
@@ -12,28 +14,46 @@ const validacionRegister = [
 
     body("email")
         .notEmpty().withMessage("Tienes que escribir un correo").bail()
-        .isEmail().withMessage("Tienes que escribir un correo válido"),
+        .isEmail().withMessage("Tienes que escribir un correo válido").bail()
+        .custom(async (val, {req}) =>  {
+            let mailCompare = req.body.email
+                let exists = await axios.get("http://127.0.0.1:3000/api/users/mailing/"+ mailCompare )
+                if(exists.data.data == null){
+                    return true
+                }
+                else{
+                     throw new Error("El mail ya esta registrado")
+                }
+        }),
 
     body("idCategory").notEmpty().withMessage("Tienes que elegir una categoria"),
     body("password").notEmpty().withMessage("Tienes que escribir una constraseña").bail()
-        .isLength({min: 8}).withMessage("La contraseña debe contener 8 caracteres"),
-    body("avatar").custom(
-           function(value, filename) {
-                var extension = (path.extname(filename)).toLowerCase();
-                switch (extension) {
-                    case '.jpg':
-                        return '.jpg';
-                    case '.jpeg':
-                        return '.jpeg';
-                    case  '.png':
-                        return '.png';
-                    case  '.gif':
-                        return '.gif';
-                    default:
-                        return true;
+        .isLength({min: 8}).withMessage("La contraseña debe contener 8 caracteres").bail()
+        .custom((val, {req})=>{
+            let pwd = req.body.password;
+            let regularExpression = /^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20})$/
+            if(!regularExpression.test(pwd)){
+                throw new Error("El password no cumple con los requisitos de complejidad mayus,minus,numero,caracterEsp")
+            }
+            else{
+                return true
+            }
+        }),
+    
+        body("avatar").custom((val, {req}) =>{
+            let img = req.file;
+            let ext = path.extname(img.originalname)
+            let extensiones = [".jpg",".png",".gif",".jpeg"];
+            if(!img) {
+            throw new Error("Hay que cargar una imagen")
+            }
+            else{
+                if(!extensiones.includes(ext)){
+                throw new Error("Las extensiones de imagen permitidas son .jpg .png .gif y .jpeg")
                 }
             }
-    ).withMessage("La imagen contiene un formato invalido")
+            return true
+        })
   ];
   
   module.exports = validacionRegister;
